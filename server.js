579f6d8ba4d75166f1e2d2a1da13578bcb5da01f
protonaby <protonaby@gmail.com>
2020-02-19 16:32:05 +0200

    update package.json

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const app = require('./app.js');
 
 const server = http.createServer(app);
-const port = process.env.APP_PORT;
+const port = process.env.PORT;
 
 server.listen(port, '0.0.0.0', function() {
   console.log('Listening on port %d', port);
